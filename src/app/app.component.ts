806c7d315bedc582c70222a354385ec6627bc64a
EC2 Default User <ec2-user@ip-10-26-14-23.cloud.tiaa-cref.org>
2020-01-13 07:43:32 +0000

    changing title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'DOCKER AND OPENSHIFT PATHASHALA';
+  title = 'DOCKER AND KUBERNETES(K8S) PATHASHALA';
 }
